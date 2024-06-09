ad3eeb98506dede01f653a635b1bb852a3c25374
Alemi Herbert <alemiherbert@gmail.com>
2024-06-10 00:46:36 +0300

    Task 5 complete

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -16,3 +16,32 @@ def status():
         A dictionary containing the status of the API.
     """
     return {"status": "OK"}
+
+
+@app_views.route("/stats", methods=["GET"], strict_slashes=False)
+def stats():
+    """
+    Returns the count of all objects in the database.
+
+    Returns:
+        A dictionary containing the count of all objects in the database.
+    """
+    from models import storage
+    from models.base_model import BaseModel
+    from models.user import User
+    from models.place import Place
+    from models.city import City
+    from models.state import State
+    from models.amenity import Amenity
+    from models.review import Review
+
+    classes = {
+        "users": User,
+        "places": Place,
+        "cities": City,
+        "states": State,
+        "amenities": Amenity,
+        "reviews": Review
+    }
+
+    return {key: storage.count(value) for key, value in classes.items()}
