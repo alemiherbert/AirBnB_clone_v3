eee297e62d0e6b0525b73681dcf973f36c76c0eb
Alemi Herbert <alemiherbert@gmail.com>
2024-06-10 00:53:45 +0300

    modify stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,8 @@ This module contains the routes for the index of the API.
 """
 
 from api.v1.views import app_views
+from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status", methods=["GET"], strict_slashes=False)
@@ -26,22 +28,12 @@ def stats():
     Returns:
         A dictionary containing the count of all objects in the database.
     """
-    from models import storage
-    from models.base_model import BaseModel
-    from models.user import User
-    from models.place import Place
-    from models.city import City
-    from models.state import State
-    from models.amenity import Amenity
-    from models.review import Review
-
-    classes = {
-        "users": User,
-        "places": Place,
-        "cities": City,
-        "states": State,
-        "amenities": Amenity,
-        "reviews": Review
+    count_stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
     }
-
-    return {key: storage.count(value) for key, value in classes.items()}
+    return jsonify(count_stats)
