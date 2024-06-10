a7960f4027b730d70fafd55011381703f7150eed
Alemi Herbert <alemiherbert@gmail.com>
2024-06-10 16:21:23 +0300

    Add tests

--- tests/test_api/test_v1/test_views/test_index.py
+++ tests/test_api/test_v1/test_views/test_index.py
@@ -1,39 +1,41 @@
 #!/usr/bin/python3
-
-"""
-This module contains the unit tests for the API endpoints
-in the v1 version of the AirBnB clone.
-"""
-
-from api.v1.app import *
+'''testing the index route'''
 import unittest
 import pep8
 from os import getenv
 import requests
 import json
+from api.v1.app import *
 
 
 storage = getenv("HBNB_TYPE_STORAGE")
 
 
 class TestIndex(unittest.TestCase):
-    """Test the index file of the API."""
-
-    def test_status(self):
-        """Test the /status route of the API."""
+    '''test index'''
+    def test_status(self):        
+        '''test status function'''
         with app.test_client() as c:
             resp = c.get('/api/v1/status')
             data = json.loads(resp.data.decode('utf-8'))
             self.assertEqual(data, {'status': 'OK'})
 
-    def test_stats(self):
-        """Test the /stats route of the API."""
+
+    def test_count(self):
+        '''test count'''
         with app.test_client() as c:
             resp = c.get('/api/v1/stats')
             data = json.loads(resp.data.decode('utf-8'))
             for k, v in data.items():
                 self.assertIsInstance(v, int)
                 self.assertTrue(v >= 0)
+
+    def test_404(self):
+        '''test for 404 error'''
+        with app.test_client() as c:
+            resp = c.get('/api/v1/yabbadabbadoo')
+            data = json.loads(resp.data.decode('utf-8'))
+            self.assertEqual(data, {"error": "Not found"})
 
 
 if __name__ == '__main__':
